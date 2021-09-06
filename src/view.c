c09d6147b9fccfefa05eb8025455a21948fd7b3f
Erfan Abdi <erfangplus@gmail.com>
2021-09-06 14:45:41 +0430

    HACK: view: Send activated signal for waydroid windows too
    Co-authored-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/view.c
+++ src/view.c
@@ -615,7 +615,8 @@ phoc_view_activate (PhocView *self, bool activate)
   g_assert (PHOC_IS_VIEW (self));
   priv = phoc_view_get_instance_private (self);
 
-  if (!desktop->maximize)
+  /* UGLY: waydroid windows always need the activate signal */
+  if ((priv->app_id && g_str_has_prefix (priv->app_id, "waydroid.")) || !desktop->maximize)
     phoc_view_appear_activated (self, activate);
 
   if (priv->toplevel_handle)
