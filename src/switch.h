5f3854d91fc4cb52d81eafbc071b7d5b7b506037
Eugenio Paolantonio (g7) <me@medesimo.eu>
2023-04-05 20:04:24 +0200

    DO NOT MERGE: phosh: forward switch events
    Allow forwarding switch events to the bound shell.
    
    Please do not merge, nor try to upstream it. I will do it properly (including
    splitting commits) in the near future (phoc master is undergoing
    much restructuring but we'll stay with 0.9.0 in bullseye).
    
    This won't be the final version - for example the new PhocSwitch class
    doesn't actually emit property changes signals - which is useless. Event
    forwarding is still done by phoc_switch_handle_toggle (previously
    roots_switch_handle_toggle).
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/switch.h
+++ src/switch.h
@@ -4,16 +4,35 @@
 
 G_BEGIN_DECLS
 
-typedef struct phoc_switch {
-  PhocSeat *seat;
-  struct wlr_input_device *device;
-  struct wl_listener device_destroy;
+#define PHOC_TYPE_SWITCH phoc_switch_get_type ()
+G_DECLARE_FINAL_TYPE (PhocSwitch, phoc_switch, PHOC, SWITCH, GObject)
 
+typedef enum _PhocSwitchType
+{
+  PHOC_SWITCH_NONE,
+  PHOC_SWITCH_LID,
+  PHOC_SWITCH_TABLET_MODE,
+  PHOC_SWITCH_KEYPAD_SLIDE,
+} PhocSwitchType;
+
+struct _PhocSwitch {
+  GObject parent;
+
+  struct wl_listener device_destroy;
   struct wl_listener toggle;
   struct wl_list link;
-} PhocSwitch;
 
-void phoc_switch_handle_toggle(struct phoc_switch *switch_device,
-                               struct wlr_event_switch_toggle *event);
+  guint switch_type;
+  guint state;
+
+  PhocSeat *seat;
+  struct wlr_input_device *device;
+};
+
+PhocSwitch *phoc_switch_new (struct wlr_input_device *device,
+                             PhocSeat *seat);
+
+void phoc_switch_handle_toggle(PhocSwitch *switch_device,
+		struct wlr_event_switch_toggle *event);
 
 G_END_DECLS
