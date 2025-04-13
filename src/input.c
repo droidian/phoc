936948a7662a0295c1d62d56e2f1d3589c3bb2f1
Eugenio Paolantonio (g7) <me@medesimo.eu>
2025-04-13 19:06:40 +0200

    Revert "treewide: Adjust to removal of input device vendor/product"
    This reverts commit 959d9b6e7f7e7b0fe8284d88c1fb6d8db6f7a3ec.

--- src/input.c
+++ src/input.c
@@ -94,7 +94,8 @@ handle_new_input (struct wl_listener *listener, void *data)
     return;
   }
 
-  g_debug ("New input device: %s %s seat:%s", device->name,
+  g_debug ("New input device: %s (%d:%d) %s seat:%s", device->name,
+           device->vendor, device->product,
            phoc_input_get_device_type (device->type), seat_name);
 
   phoc_seat_add_device (seat, device);
--- src/virtual.c
+++ src/virtual.c
@@ -39,7 +39,8 @@ phoc_handle_virtual_pointer (struct wl_listener *listener, void *data)
   seat = phoc_input_get_seat (input, seat_name);
   g_return_if_fail (seat);
 
-  g_debug ("New virtual input device: %s %s seat:%s", device->name,
+  g_debug ("New virtual input device: %s (%d:%d) %s seat:%s", device->name,
+           device->vendor, device->product,
            phoc_input_get_device_type (device->type), seat_name);
 
   phoc_seat_add_device (seat, device);
